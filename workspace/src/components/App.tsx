cf2c47d22e0e5fafb5ec71dee84aa1f17bdcb227
Nils Hartmann <nils@nilshartmann.net>
2026-08-21 18:11:09 +0200

    02: Props

--- workspace/src/components/App.tsx
+++ workspace/src/components/App.tsx
@@ -3,7 +3,18 @@ import PlantCard from "./PlantCard.tsx";
 export default function App() {
   return (
     <div className={"AppContainer"}>
-      <PlantCard />
+      <PlantCard
+        name={"Aloe Vera"}
+        location={"Schlafzimmer"}
+        wateringInterval={12}
+        lastWatered={"2026-08-28"}
+      />
+
+      <PlantCard
+        name={"Orchidee"}
+        location={"Wohnzimmer"}
+        wateringInterval={20}
+      />
     </div>
   );
 }
--- workspace/src/components/PlantCard.tsx
+++ workspace/src/components/PlantCard.tsx
@@ -1,13 +1,30 @@
-export default function PlantCard() {
+type PlantCardProps = {
+  name: string;
+  location: string;
+  wateringInterval: number;
+  lastWatered?: string;
+};
+
+export default function PlantCard({
+  name,
+  location,
+  wateringInterval,
+  lastWatered,
+}: PlantCardProps) {
+  const wateringInfo =
+    wateringInterval === 1
+      ? "Jeden Tag gießen!"
+      : `Alle ${wateringInterval} Tage gießen`;
+
   return (
     <div className={"PlantCard"}>
       <header>
-        <h2>Aloe Vera</h2>
-        <div>📍Schlafzimmer</div>
+        <h2>{name}</h2>
+        <div>📍{location}</div>
       </header>
       <section>
-        <div>Alle 5 Tage gießen</div>
-        <div>Zuletzt: 24.06.2026</div>
+        <div>{wateringInfo}</div>
+        {lastWatered ? <div>Zuletzt: {lastWatered}</div> : null}
       </section>
     </div>
   );
